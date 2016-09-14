75ed50f1839286312da40ae2356805ca23b0d7f9
JR Bing <jr@jrbing.com>
2016-09-14 15:55:36 -0400

    Disable transparent hugepages and tuned daemon

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -19,7 +19,21 @@ function use_redhat_kernel() {
   fi
 }
 
+function disable_transparent_hugepages() {
+  echoinfo "Disabling transparent hugepages"
+  sed -i 's/^GRUB_CMDLINE_LINUX.*quiet/& transparent_hugepages=never/' /etc/default/grub
+  grub2-mkconfig -o /boot/grub2/grub.cfg
+}
+
+function disable_tuned() {
+  echoinfo "Disabling the tuned service"
+  systemctl stop tuned
+  systemctl disable tuned
+}
+
 use_redhat_kernel
+disable_transparent_hugepages
+disable_tuned
 
 echoinfo "Rebooting the machine"
 reboot
