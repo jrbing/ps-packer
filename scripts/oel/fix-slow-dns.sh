3e61e4af1acf6028da434be0e54a7ce5736aa58c
JR Bing <jr@jrbing.com>
2016-11-22 13:04:36 -0500

    Switch over to using systemd

--- scripts/oel/fix-slow-dns.sh
+++ scripts/oel/fix-slow-dns.sh
@@ -15,7 +15,7 @@ function apply_slow_dns_fix() {
   # http://www.linuxquestions.org/questions/showthread.php?p=4399340#post4399340
   # add 'single-request-reopen' so it is included when /etc/resolv.conf is generated
   echo 'RES_OPTIONS="single-request-reopen"' >> /etc/sysconfig/network
-  service network restart
+  systemctl restart network
 }
 
 apply_slow_dns_fix
